00509e738a5107c17249daa70a0f0dd9e8be6970
Matei1212cov <covalenco.matei@gmail.com>
2025-03-22 14:46:23 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,56 @@
+import random
+import telebot
+from telebot.types import ReactionTypeEmoji
+from telebot import custom_filters
+from bot_logic import gen_pass
+# Замени 'TOKEN' на токен твоего бота
+# Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot("7227303114:AAFxUJo5ZfI70TJ3iGQgvxu3yDB4gVBgsHo")
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+    
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+   bot.reply_to(message, "Привет! Как дела?")
+    
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['pass'])
+def send_password(message):
+    password = gen_pass(10)  # Устанавливаем длину пароля, например, 10 символов
+
+    bot.reply_to(message, f"Вот твой сгенерированный пароль: {password}")   
+    
+#@bot.message_handler(func=lambda message: True)
+#def echo_all(message):
+   # bot.reply_to(message, message.text)
+    
+#bot.polling()
+
+
+
+# Send a reactions to all messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def send_reaction(message):
+    emo = ["\U0001F525", "\U0001F917", "\U0001F60E"]  # or use ["🔥", "🤗", "😎"]
+    bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji(random.choice(emo))], is_big=False)
+
+
+@bot.message_reaction_handler(func=lambda message: True)
+def get_reactions(message):
+    bot.reply_to(message, f"You changed the reaction from {[r.emoji for r in message.old_reaction]} to {[r.emoji for r in message.new_reaction]}")
+
+
+
+
+@bot.message_handler(chat_types=['supergroup'], is_chat_admin=True)
+def answer_for_admin(message):
+    bot.send_message(message.chat.id,"hello my admin")
+
+# Register filter
+bot.add_custom_filter(custom_filters.IsAdminFilter(bot))
+bot.infinity_polling(allowed_updates=['message', 'message_reaction'])
